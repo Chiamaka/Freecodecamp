fe5f13db565afdfe0b49e56311247c9c60b22f1c
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2017-01-04 13:48:22 +0100

    Symmetric Difference solved. Hard as fuck

---
+++ advancedAlgos/Symmetric Difference 2.js
@@ -0,0 +1,45 @@
+/**
+ * Created by chiamaka on 1/4/17.
+ *
+ * Create a function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays.
+
+ Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
+
+ Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+ Here are some helpful links:
+
+ Array.prototype.reduce()
+ Symmetric Difference
+ */
+
+function sym(args) {
+    let arr = [],
+        newArr = [];
+    for (const value of arguments) {
+        arr.push(value);
+    }
+    arr.reduce(function (prev, current) {
+        const temp = [];
+        current.reduce((prev, current) => {
+            //remove all duplicates in the current array
+            if (temp.indexOf(current) === -1) {
+                temp.push(current);
+            }
+        }, 0);
+
+        temp.reduce((prev, current) => {
+            //if element is not found, push it. If it is found, delete it.
+            if (newArr.indexOf(current) === -1) {
+                newArr.push(current);
+            } else {
+                newArr.splice(newArr.indexOf(current), 1);
+            }
+        }, 0);
+    }, []);
+
+    //return a sorted array just because.
+    return newArr.sort((a,b) => a>b);
+}
+
+console.log(sym([3,3,3,2,5], [2,1,5,7], [3,4,6,6], [1,2,3], [5,3,9,8], [1]));
---
+++ advancedAlgos/Symmetric Difference.js
@@ -0,0 +1,47 @@
+/**
+ * Created by chiamaka on 12/25/16.
+ *
+ * Create a function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays.
+
+ Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
+
+ Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+ Here are some helpful links:
+
+ Array.prototype.reduce()
+ Symmetric Difference
+ */
+
+function sym(args) {
+    const arr = [],
+        newArr = [];
+
+    for (const value of arguments) {
+        arr.push(value);
+    }
+
+    arr.forEach(current => {
+        const temp = [];
+        current.forEach((value) => {
+            //remove all duplicates in the current array
+            if (temp.indexOf(value) === -1) {
+                temp.push(value);
+            }
+        });
+
+        temp.forEach((value) => {
+            //if element is not found, push it. If it is found, delete it.
+            if (newArr.indexOf(value) === -1) {
+                newArr.push(value);
+            } else {
+                newArr.splice(newArr.indexOf(value), 1);
+            }
+        });
+    });
+
+    return newArr.sort((a, b) => a > b);
+}
+
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
+
